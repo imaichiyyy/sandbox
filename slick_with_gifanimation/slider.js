43c3424751ba67b814b87a915c82a1237e6410b2
Yoichi.Imamura <yoichi.imamura@i-studio.co.jp>
2018-12-09 23:17:19 +0900

    chore: Update PrelaodImage function.

--- slick_with_gifanimation/slider.js
+++ slick_with_gifanimation/slider.js
@@ -7,21 +7,24 @@ export default class {
     this.query = 0
     this.el = {
       animation: this.element.querySelector('[data-dom="animation"]'),
-      preload: document.querySelector('[data-dom="preload"]')
+      preload: undefined,
+      preloaditem: []
     }
     this.init()
     this.bindEvents()
   }
   init(){
-    console.log('Load OK')
+    this.preloadImageInit()
+
     //initのタイミングでGifアニメをプリロードするか、スライド切替時にLazyロードした方がよいです
     this.slider = $('[data-dom="slider"]').slick()
+
     //下記関数発火で1枚目のGifアニメが動き出すので、スクロールイベントと組み合わせる
     this.startAnimation()
   }
   bindEvents(){
     this.slider.on('beforeChange', (event, slick, currentSlide)=>{
-      this.reserveNumber = (slick.slideCount-1 === currentSlide) ? 0 : currentSlide+1
+      this.reserveNumber = (slick.slideCount-1 === currentSlide) ? 0 : parseInt(currentSlide+1)
       if(this.reserveNumber !== currentSlide) {
         this.preloadImage()
       }
@@ -33,11 +36,22 @@ export default class {
       }
     })
   }
+  preloadImageInit(){
+    let preload = document.createElement('div')
+    preload.setAttribute('data-dom', 'preload')
+    document.body.appendChild(preload)
+    this.el.preload = document.querySelector('[data-dom="preload"]')
+    for(let i = 0; i < this.element.querySelectorAll('[data-role="slideritem"]').length; i=i+1){
+      let img = document.createElement('img')
+      img.setAttribute('data-role', 'preloaditem')
+      img.setAttribute('src', this.el.animation.getAttribute('src'))
+      this.el.preload.appendChild(img)
+      this.el.preloaditem.push(img)
+    }
+  }
   preloadImage(){
     this.query = this.ramdomQuery()
-    let img = document.createElement('img')
-    img.setAttribute('src', 'anime' + this.reserveNumber + '.gif?r=' + this.query)
-    this.el.preload.appendChild(img)
+    this.el.preloaditem[this.reserveNumber].setAttribute('src', 'anime' + this.reserveNumber + '.gif?r=' + this.query)
   }
   startAnimation(){
     this.el.animation.setAttribute('src', 'anime' + this.currentNumber + '.gif?r=' + this.query)
